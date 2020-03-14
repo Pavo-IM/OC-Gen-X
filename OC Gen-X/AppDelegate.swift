b2ae3a2befcd8beb0e9670458f9eca2055a127ad
Pavo <brockbackups@gmail.com>
2020-03-14 19:40:07 -0400

    Initial Commit

---
+++ OC Gen-X/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OC Gen-X
+//
+//  Created by Henry Brock on 3/14/20.
+//  Copyright © 2020 Henry Brock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OC Gen-X/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OC Gen-X
+//
+//  Created by Henry Brock on 3/14/20.
+//  Copyright © 2020 Henry Brock. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
