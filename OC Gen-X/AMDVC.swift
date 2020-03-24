88533cf6aff27abdb7723602afe5f5c62b9ee115
Pavo <brockbackups@gmail.com>
2020-03-24 10:12:23 -0400

    updates

--- OC Gen-X/AMDVC.swift
+++ OC Gen-X/AMDVC.swift
@@ -4,13 +4,17 @@ class AMDVC: NSViewController {
     @IBOutlet weak var amdChipsetList: NSPopUpButton!
     
     var moboFamily = ["15/16h", "17h"]
+    var selection: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         amdChipsetList.removeAllItems()
         amdChipsetList.addItems(withTitles: moboFamily)
+        
+        selection = amdChipsetList.titleOfSelectedItem
     }
+    
     @IBAction func previousClicked(_ sender: Any) {
         if let mainWC = view.window?.windowController as? MainWindowController {
             mainWC.moveToMainVC()
--- OC Gen-X/kextsVC.swift
+++ OC Gen-X/kextsVC.swift
@@ -1,10 +1,13 @@
 import Cocoa
 
 class kextsVC: NSViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        print()
+
     }
     @IBAction func previousClicked(_ sender: Any) {
         if let mainWC = view.window?.windowController as? MainWindowController {
