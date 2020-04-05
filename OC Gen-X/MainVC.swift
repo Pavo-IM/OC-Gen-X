fd2f08b4c3491aa37ac09899f1456d5bb26d6bf1
Pavo <brockbackups@gmail.com>
2020-04-05 11:14:38 -0400

    Updated UI

--- OC Gen-X/MainVC.swift
+++ OC Gen-X/MainVC.swift
@@ -2,15 +2,16 @@ import Cocoa
 
 class MainVC: NSViewController {
     @IBOutlet weak var ivyBridgeChecked: NSButton!
-    @IBOutlet weak var amd15_16Checked: NSButton!
     @IBOutlet weak var haswellChecked: NSButton!
     @IBOutlet weak var skylakeChecked: NSButton!
     @IBOutlet weak var kabyLakeChecked: NSButton!
-    @IBOutlet weak var coffeeLakeChecked: NSButton!
     @IBOutlet weak var haswellEChecked: NSButton!
     @IBOutlet weak var broadwellEChecked: NSButton!
     @IBOutlet weak var skylakeXChecked: NSButton!
-    @IBOutlet weak var amd17Checked: NSButton!
+    @IBOutlet weak var ryzenChecked: NSButton!
+    @IBOutlet weak var coffeLakeChecked: NSButton!
+    @IBOutlet weak var fxChecked: NSButton!
+    @IBOutlet weak var threadripperChecked: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
